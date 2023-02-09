ae643f7aa285bef0870ed0d0787921423d3eaf41
Ashneal Powell <pashneal@gmail.com>
2023-02-09 18:59:36 -0500

    Rebrand

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use yew::{Properties, function_component, Html, html};
+use yew::prelude::*;
+use wasm_bindgen::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
+#[wasm_bindgen]
+pub fn main() {
+    yew::Renderer::<App>::new().render();
+}
